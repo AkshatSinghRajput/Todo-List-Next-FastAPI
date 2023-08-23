9e528e9d1ab6724a7bc77c8ef5bff67086e67905
Akshat Singh Rajput <choturajput0@gmail.com>
2023-08-23 20:04:54 +0530

    Client: TodoForm and UI

--- client/src/Components/TodoContainer.tsx
+++ client/src/Components/TodoContainer.tsx
@@ -4,11 +4,11 @@ import TodoForm from "./TodoForm";
 
 export default function TodoContainer() {
   return (
-    <div className="mt-2 w-full flex flex-col min-h-screen justify-between">
-      <div className="h-[75vh]"></div>
-      <div className="w-[80%] h-[20vh] mx-2">
+    <div className="mt-2 w-full flex flex-col min-h-screen justify-between items-center">
+      <div className="w-[97%] h-[20vh] mx-2">
         <TodoForm />
       </div>
+      <div className="h-[75vh]"></div>
     </div>
   );
 }
--- client/src/Components/TodoForm.tsx
+++ client/src/Components/TodoForm.tsx
@@ -5,35 +5,68 @@ import React, { useState } from "react"; // Import React from the correct packag
 import { AiOutlinePlus } from "react-icons/ai";
 
 export default function TodoForm() {
-  const [formActive, setFormActive] = useState(false);
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [important, setImportant] = useState(false);
+
+  const handleForm = (e: Event) => {
+    e.preventDefault();
+    console.log(
+      `Title: ${title}, Description: ${description}, Important: ${important}`
+    );
+  };
+
   return (
     <>
       <Card>
-        <form className="flex flex-row w-full items-center">
-          <div>
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <p>Create Your Todos Here</p>
+        </h5>
+        <form className="flex lg:flex-row flex-col w-full items-center justify-center gap-2">
+          <div className="lg:w-[40%] w-full">
             <div className="mb-2 block">
-              <Label htmlFor="email1" value="Your email" />
+              <Label htmlFor="title1" value="Title" />
             </div>
             <TextInput
-              id="email1"
-              placeholder="name@flowbite.com"
+              id="title1"
+              placeholder="Todo Title"
               required
-              type="email"
+              type="text"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
             />
           </div>
-          <div>
+          <div className="lg:w-[40%] w-full">
             <div className="mb-2 block">
-              <Label htmlFor="password1" value="Your password" />
+              <Label htmlFor="description1" value="Description" />
             </div>
-            <TextInput id="password1" required type="password" />
+            <TextInput
+              id="description1"
+              placeholder="Todo Description"
+              required
+              type="text"
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+            />
           </div>
-          <div className="flex items-center gap-2">
-            <Checkbox id="remember" />
-            <Label htmlFor="remember">Remember me</Label>
+          <div className="flex gap-2">
+            <div className="flex flex-col gap-2 justify-center items-center">
+              <Label htmlFor="remember">Important</Label>
+              <Checkbox
+                id="remember"
+                className="lg:w-[50%] lg:h-[63%]"
+                onChange={() => setImportant(!important)}
+              />
+            </div>
+            <Button
+              type="button"
+              className=" bg-primary-600 mt-7"
+              onClick={handleForm}
+            >
+              <AiOutlinePlus className="mr-2 h-5 w-5"></AiOutlinePlus>
+              <p>Add</p>
+            </Button>
           </div>
-          <Button type="submit" className=" bg-primary-600">
-            Submit
-          </Button>
         </form>
       </Card>
     </>
